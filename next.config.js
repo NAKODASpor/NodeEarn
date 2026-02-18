dd947e5b29ff76080b255cfe5377316de9cc5aa0
NAKODASpor <irebornid@gmail.com>
2026-02-18 08:02:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Mengaktifkan fitur React Strict Mode untuk mendeteksi potensi masalah pada kode
-  reactStrictMode: true,
-  
-  // Mengizinkan pengambilan gambar dari domain luar (penting untuk login Google/Avatar)
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
+const nextConfig = {};
 
-  // Opsional: Jika kamu ingin aplikasi tetap jalan meski ada error ESLint saat build di Vercel
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
